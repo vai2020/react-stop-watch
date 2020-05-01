1c72b15c3fc789e715269f4421de2d9703f794fe
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-30 20:17:17 -0500

    we set up counter to begin counting

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -8,11 +8,13 @@ class App extends Component {
     //the following block of codes is to initialize state, setting counter to zero//
         this.state = {counter:0  }
 
-  }
+  }  // the following block of codes is for handling the start, pause and reset buttons//
+        
+
   render() {
     return (
       <div>
-       <button className="startbtn"> Start</button>
+       <button className="startbtn" onClick={beginCounter}> Start</button>
        <button className="pausebtn"> Pause</button>
        <button className="resetbtn"> Reset</button>
       

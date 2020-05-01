eb3c91e715461fb41b81aa845b360d63463c787c
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-30 20:00:37 -0500

    first commit

---
+++ stopwatch/src/App.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+        this.state = {  }
+
+  }
+  render() {
+    return (
+      <div>
+       <button className="startbtn"> Start</button>
+       <button className="pausebtn"> Pause</button>
+       <button className="resetbtn"> Reset</button>
+      
+         
+       
+       
+      </div>
+    )
+  }
+
+
+
+}
+
+    
+  
+  
+
+export default App;
